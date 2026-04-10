ca18deacca524d664ad0fc48ac24d9fc4f4cbd63
Robby Wu <ta7382@gmail.com>
2026-04-10 17:00:43 +0800

    feat: enhance ScrollToTop component to stop player on route change

--- src/components/scroll_to_top.tsx
+++ src/components/scroll_to_top.tsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
+import { usePlayerStore } from "../stores/player_store";
+
 export function ScrollToTop() {
   const { pathname } = useLocation();
+  const stop = usePlayerStore((s) => s.stop);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+    stop();
+  }, [pathname, stop]);
 
   return null;
 }
